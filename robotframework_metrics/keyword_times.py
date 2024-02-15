4ffb41fe65211c35f07a4616f41ac854b653e7ab
Shiva Prasad Adirala <adiralashiva8@gmail.com>
2024-02-15 15:06:24 +0530

    RF7 Support

--- robotframework_metrics/keyword_times.py
+++ robotframework_metrics/keyword_times.py
@@ -5,8 +5,8 @@ class KeywordTimes():
     def get_keyword_times(self, kw_list):
         keywords_data_frame = pd.DataFrame.from_records(kw_list)
         if not keywords_data_frame.empty:
-            kw_times = (keywords_data_frame.groupby("Name").agg(times = ("Time", "count"), time_min = ("Time", min),
-            time_max = ("Time", max), time_mean = ("Time", "mean")).reset_index())
+            kw_times = (keywords_data_frame.groupby("Name").agg(times = ("Time", "count"), time_min = ("Time", "min"),
+            time_max = ("Time", "max"), time_mean = ("Time", "mean")).reset_index())
         else:
             kw_times = keywords_data_frame
         return kw_times
