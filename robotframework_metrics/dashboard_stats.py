61b5e127ed03f84f6e9d6633398c1496e9646f22
Shiva Prasad Adirala <adiralashiva8@gmail.com>
2024-10-23 20:11:12 +0530

    include execution, time stats in dashboard

--- robotframework_metrics/dashboard_stats.py
+++ robotframework_metrics/dashboard_stats.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import datetime
+import numpy as np
 
 
 class Dashboard:
@@ -55,17 +57,6 @@ class Dashboard:
             }
         return kw_stats
 
-    def group_error_messages(self, test_list):
-        test_data_frame = pd.DataFrame.from_records(test_list)
-        grouped_data_frame = (
-            test_data_frame.groupby("Message", as_index=False)  # Keep 'Message' as a column
-            .agg(times=("Status", "count"))  # Count occurrences of each 'Status'
-            .sort_values(by=['times'], ascending=False, ignore_index=True)  # Sort by count
-        )
-
-        return grouped_data_frame.head(6)
-        # return (test_data_frame.groupby("Message", as_index=False).agg(times = ("Status", "count")).head(6).reset_index()).sort_values(by = ['times'], ascending = [False], ignore_index=True)
-
     def suite_error_statistics(self, suite_list):
         suite_data_frame = pd.DataFrame.from_records(suite_list)
         required_data_frame = pd.DataFrame(suite_data_frame, columns = ['Name', 'Total', 'Fail'])
@@ -73,3 +64,23 @@ class Dashboard:
         filtered_data_frame = required_data_frame[required_data_frame['Fail'] > 0]
         # print(required_data_frame)
         return filtered_data_frame.sort_values(by = ['Fail'], ascending = [False], ignore_index=True).head(10).reset_index(drop=True)
+
+    def get_execution_info(self, test_list):
+        data_frame = pd.DataFrame.from_records(test_list)
+        data_frame['start_time'] = pd.to_datetime(data_frame['start_time'])
+        data_frame['end_time'] = pd.to_datetime(data_frame['end_time'])
+        initial_start_time = data_frame['start_time'].min()
+        final_end_time = data_frame['end_time'].max()
+        overall_execution_time = final_end_time - initial_start_time
+        return [initial_start_time, final_end_time, overall_execution_time]
+
+    def get_test_execution_trends(self, test_list):
+        data_frame = pd.DataFrame.from_records(test_list)
+        num_bins = 10
+        min_time = round(data_frame['Time'].min()/60000, 2)
+        max_time = round(data_frame['Time'].max()/60000, 2)
+        bins = np.linspace(min_time, max_time, num_bins + 1)
+        labels = [f'{round(bins[i], 0)} - {round(bins[i+1], 0)} min' for i in range(len(bins)-1)]
+        data_frame['time_group'] = pd.cut(round(data_frame['Time']/60000,2), bins=bins, labels=labels, include_lowest=True)
+        result = data_frame.groupby('time_group').size().reset_index(name='test_case_count')
+        return result
